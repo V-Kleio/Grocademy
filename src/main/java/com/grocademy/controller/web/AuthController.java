5ab0918d6f91be19e1d91c20fa5d107c5b3d4da1
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-08-21 21:37:19 +0700

    feat(controller): Add user registration

---
+++ src/main/java/com/grocademy/controller/web/AuthController.java
@@ -0,0 +1,43 @@
+package com.grocademy.controller.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.grocademy.dto.UserDto;
+import com.grocademy.service.UserService;
+
+@Controller
+public class AuthController {
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new UserDto("", "", "", "", ""));
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUserAccount(@ModelAttribute("user") UserDto registrationDto) {
+        try {
+            userService.registerUser(
+                registrationDto.firstName(),
+                registrationDto.lastName(),
+                registrationDto.username(),
+                registrationDto.email(),
+                registrationDto.password()
+            );
+        } catch (IllegalStateException e) {
+            return "redirect:/register?error";
+        }
+        return "redirect:/login";
+    }
+}
