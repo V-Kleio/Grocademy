02c2b4751257d3797dfe0b0a2f19b9a07ade0cb0
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-08-22 01:38:44 +0700

    feat(repository): Add PurchasedCourseRepository

---
+++ src/main/java/com/grocademy/repository/PurchasedCourseRepository.java
@@ -0,0 +1,15 @@
+package com.grocademy.repository;
+
+import com.grocademy.entity.PurchasedCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface PurchasedCourseRepository extends JpaRepository<PurchasedCourse, Long> {
+    @Query("SELECT pc.course.id FROM PurchasedCourse pc WHERE pc.user.id = :userId")
+    Set<Long> findCourseIdsByUserId(@Param("userId") Long userId);
+}
