888e88c5d120c84cbe1cd73185d123fcb59444e9
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-08-21 21:38:44 +0700

    style(config): Remove unnecessary newline

--- src/main/java/com/grocademy/config/SecurityConfig.java
+++ src/main/java/com/grocademy/config/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/login?logout")
                 .permitAll()
             );
-            
         return http.build();
     }
 }
