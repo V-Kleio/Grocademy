ec706cde4d086bb92000d32c2bb1753d106599c6
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-08-24 01:48:16 +0700

    feat(repository): Add additional user query

--- src/main/java/com/grocademy/repository/UserRepository.java
+++ src/main/java/com/grocademy/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.grocademy.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.grocademy.entity.User;
@@ -9,4 +11,6 @@ import com.grocademy.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Page<User> findByUsernameContainingIgnoreCaseOrFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String username, String firstName, String lastName, String email, Pageable pageable);
 }
