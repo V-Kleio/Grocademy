004729395394c50c5c3ed5ec0f56095a313eace0
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-08-24 01:44:33 +0700

    feat(repo): Add pagination support for module repo

--- src/main/java/com/grocademy/repository/ModuleRepository.java
+++ src/main/java/com/grocademy/repository/ModuleRepository.java
@@ -1,9 +1,17 @@
 package com.grocademy.repository;
 
 import com.grocademy.entity.Module;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ModuleRepository extends JpaRepository<Module, Long> {
     List<Module> findAllByCourseIdOrderByModuleOrderAsc(Long courseId);
+    int countByCourseId(Long courseId);
+
+    Page<Module> findAllByCourseIdOrderByModuleOrderAsc(@Param("courseId") Long courseId, Pageable pageable);
 }
