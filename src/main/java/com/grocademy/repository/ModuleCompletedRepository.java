2ae85e43b9966ae80910cd0416499c18d8db37d0
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-08-24 01:44:07 +0700

    fix(repository): Fix module completed query

--- src/main/java/com/grocademy/repository/ModuleCompletedRepository.java
+++ src/main/java/com/grocademy/repository/ModuleCompletedRepository.java
@@ -3,10 +3,15 @@ package com.grocademy.repository;
 import com.grocademy.entity.ModuleCompleted;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.Set;
 
 public interface ModuleCompletedRepository extends JpaRepository<ModuleCompleted, Long> {
-    @Query("SELECT mc.module.id FROM ModuleCompletion mc WHERE mc.user.id = :userId AND mc.module.course.id = :courseId")
-    Set<Long> findCompletedModuleIdByUserIdAndCourseId(Long userId, Long courseId);
+    @Query("SELECT mc.module.id FROM ModuleCompleted mc " +
+           "WHERE mc.user.id = :userId AND mc.module.course.id = :courseId")
+    Set<Long> findCompletedModuleIdByUserIdAndCourseId(
+        @Param("userId") Long userId,
+        @Param("courseId") Long courseId
+    );
     boolean existsByUserIdAndModuleId(Long userId, Long moduleId);
 }
